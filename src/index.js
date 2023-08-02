117b16376bc8135320fab938a5bdbe928821ec58
Святослав Минчаков <sminchakov@gmail.com>
2023-08-02 23:46:42 +0300

    created button btnShowMore

--- src/index.js
+++ src/index.js
@@ -5,22 +5,24 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 const API_KEY = '35528535-2026f3bafef7be5a50534f79c';
 const BASE_URL = 'https://pixabay.com/api/';
 let enterValue = '';
-
+let pageNumber = 1;
 const imageBox = document.querySelector('.gallery');
 const pageform = document.querySelector('.search-form');
 const pageInput = document.querySelector('input');
+const btnShowMore = document.querySelector('.show-more');
 
 pageInput.addEventListener('input', (event) => {
   enterValue = event.target.value;
 });
 
 async function getImageCollection() {
-  return await axios.get(`${BASE_URL}?key=${API_KEY}&q=${enterValue}&image_type=photo`);
+  return await axios.get(`${BASE_URL}?key=${API_KEY}&q=${enterValue}&image_type=photo&orientation=horizontal&safesearch=true&page=${pageNumber}&per_page=40`);
 }
 
 function renderImageCollection() {
   getImageCollection().then(response => {
     const data = response.data;
+    
     if (data.total === 0 || !enterValue) {
       Notify.warning('По вашому запиту нічого не знайдено!',
         {
@@ -32,7 +34,15 @@ function renderImageCollection() {
       pageInput.value = '';
       return
     }
-
+    console.log(data)
+    Notify.success(`Знайдено ${data.totalHits} зображень`,
+        {
+          timeout: 5000,
+          position: "center-center",
+          width: '400px',
+          fontSize: '18px'
+        });
+    if (data.hits.length === 40) btnShowMore.classList.remove('is-hidden');
     data.hits.map(card => {
       const url = card.webformatURL;
       const image = `<div class="photo-card">
@@ -53,6 +63,7 @@ function renderImageCollection() {
         </div>
       </div>`;
       imageBox.insertAdjacentHTML('beforeend', image);
+      pageInput.value = '';
     });
   });
 }
@@ -65,3 +76,37 @@ pageform.addEventListener('submit', (event) => {
     renderImageCollection(); 
   
 });
+
+btnShowMore.addEventListener('click', renderNextPage);
+
+function renderNextPage() {
+  pageNumber += 1;
+  
+  getImageCollection().then(response => {
+    const data = response.data;
+    
+    if (data.hits.length < 40) btnShowMore.classList.add('is-hidden');
+    data.hits.map(card => {
+      const url = card.webformatURL;
+      const image = `<div class="photo-card">
+        <img width="350" height="233" src="${url}" alt="${card.tags}" loading="lazy" />
+        <div class="info">
+          <p class="info-item">
+            <b>Likes:</b>  ${card.likes}
+          </p>
+          <p class="info-item">
+            <b>Views:</b>  ${card.views}
+          </p>
+          <p class="info-item">
+            <b>Comments:</b>  ${card.comments}
+          </p>
+          <p class="info-item">
+            <b>Downloads:</b>  ${card.downloads}
+          </p>
+        </div>
+      </div>`;
+      imageBox.insertAdjacentHTML('beforeend', image);
+      pageInput.value = '';
+    });
+  });
+}
